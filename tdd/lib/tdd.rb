545b96e9bd594598cb428619e8d9db14284b9410
sarah_peters <sarah.kathryn.peters@gmail.com>
2018-10-31 13:52:11 -0700

    Add #my_transpose and #stock_picker

--- tdd/lib/tdd.rb
+++ tdd/lib/tdd.rb
@@ -12,3 +12,28 @@ def two_sum(array)
   end
   result
 end
+
+def my_transpose(array)
+  result = Array.new(3) { Array.new [] }
+
+  (0..array.length - 1).each do |i|
+    array.each do |arr|
+      result[i] << arr[i]
+    end
+  end
+  result
+end
+
+def stock_picker(prices)
+  days = []
+  (0..prices.length - 1).each do |first|
+    (first + 1..prices.length - 1).each do |second|
+      greatest_return = 0
+      if (prices[first] - prices[second]).abs > greatest_return
+        greatest_return = (prices[first] - prices[second]).abs
+        days = [first, second]
+      end
+    end
+  end
+  days
+end
--- tdd/spec/tdd_spec.rb
+++ tdd/spec/tdd_spec.rb
@@ -16,4 +16,34 @@ RSpec.describe do
       expect(two_sum(array)).to eq([[0, 4], [2, 3]])
     end
   end
+
+  describe "my_transpose" do
+    subject(:array) {Array.new([
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8]
+    ])}
+    answer =  [[0, 3, 6],
+                [1, 4, 7],
+                [2, 5, 8]]
+
+    it "converts between row and column representations" do
+      expect(my_transpose(array)).to eq(answer)
+    end
+  end
+
+  describe "#stock_picker" do
+    subject(:prices) {Array.new([5, 10, 8, 4, 12])}
+
+    it "it outputs the most profitable pair of days on which to buy the stock" do
+      expect(stock_picker(prices)).to eq([3,4])
+    end
+
+    it "cannot sell stock before buying it" do
+      sell = stock_picker(prices).last
+      expect(stock_picker(prices).first).to be < sell
+    end
+  end
+
+  
 end
